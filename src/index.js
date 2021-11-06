1e71d44d5b60e0c83cb9f19eee84b316cfd53fb2
José Marinho <eduardo.u.marinho@gmail.com>
2021-11-06 22:33:41 +0000

    start

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import utils from './math-utils';
+
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="square" style={{ width: `${this.props.size}px` }}>
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+class Board extends React.Component {
+
+
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+        const maxWidth = 220
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-list" style={{ maxWidth: `${maxWidth}px` }}>
+                    {utils.range(1, 9).map(nr =>
+                        <Square key={nr} value={nr} size={maxWidth / 3} />
+                    )}
+                </div>
+
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Game />,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
